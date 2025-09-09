f51073ed348033c442f04906525dcf14b10529cc
Beata <59803918+bea94k@users.noreply.github.com>
2025-09-09 12:57:43 +0300

    Use SWR to fetch data

--- hat-menu/src/pages/RecipesPage.tsx
+++ hat-menu/src/pages/RecipesPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import useSWR from 'swr';
+import fetcher from '../data/fetcher';
 
 const RecipesPage: React.FC = () => {
+    const { data, error, isLoading } = useSWR('http://localhost:3000/recipes', fetcher);
+
+    if (error) return <div>failed to load</div>;
+    if (isLoading) return <div>loading...</div>;
+ 
+
+
     return (
         <main id="maincontent">
             <h1>browsing recipes here</h1>
+            <p> first recipe: {data[0].name}.</p>
         </main>
     );
 };
