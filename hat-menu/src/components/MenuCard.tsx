73d991dd778769c70a650cf17679df51c5973c9f
mikerinen <mhyvarinen@protonmail.com>
2025-10-26 13:57:23 +0200

    Add recipe names and weekdays in MenuCard

--- hat-menu/src/components/MenuCard.tsx
+++ hat-menu/src/components/MenuCard.tsx
@@ -1,22 +1,32 @@
-import { format } from 'date-fns';
+import { format, addDays } from 'date-fns';
 import type { Menu } from '../schemas/Menus';
 
 interface Props {
     menu: Menu;
 }
 
-const MenuCard = ({ menu }: Props) => (
-    <div>
-        Menu{' '}
-        <span style={{ fontWeight: 'bold' }}>{menu.startDate && format(menu.startDate, 'd MMM yyyy')}</span>
-        {' '}to{' '}
-        <span style={{ fontWeight: 'bold' }}>{menu.endDate && format(menu.endDate, 'd MMM yyyy')}</span>
-        <ol>
-            {menu.recipes.map((recipe, i) => (
-                <li key={`${recipe}-${i}`}>Recipe ID: {recipe.id}</li>
-            ))}
-        </ol>
-    </div>
-);
+const MenuCard = ({ menu }: Props) => {
+    const startDate = menu.startDate;
+    
+    return (
+        <div>
+            Menu{' '}
+            <span style={{ fontWeight: 'bold' }}>{startDate && format(startDate, 'd MMM yyyy')}</span>
+            {' '}to{' '}
+            <span style={{ fontWeight: 'bold' }}>{menu.endDate && format(menu.endDate, 'd MMM yyyy')}</span>
+            <ol>
+                {menu.recipes.map((recipe, i) => {
+                    const date = startDate ? addDays(startDate, i) : null;
+                    const weekday = date ? format(date, 'EEE') : '';
+                    return (
+                        <li key={`${recipe.id}-${i}`}>
+                            {weekday && <span style={{ fontWeight: 'bold' }}>{weekday}:</span>} {recipe.name}
+                        </li>
+                    );
+                })}
+            </ol>
+        </div>
+    );
+};
 
 export default MenuCard;
