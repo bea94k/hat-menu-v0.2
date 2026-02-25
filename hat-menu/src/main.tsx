0b95e3044688a7c38299874e7699c88ad7e7ce66
Beata <59803918+bea94k@users.noreply.github.com>
2026-02-25 11:45:36 +0200

    Use the protected routes, sign up and sign in in main, update navbar to display correct links depending on auth

--- hat-menu/src/main.tsx
+++ hat-menu/src/main.tsx
@@ -7,17 +7,28 @@ import RecipesPage from './pages/RecipesPage.tsx';
 import AddRecipePage from './pages/AddRecipePage.tsx';
 import MenusPage from './pages/MenusPage.tsx';
 import CreateMenuPage from './pages/CreateMenuPage.tsx';
+import AuthProvider from './auth/AuthProvider.tsx';
+import ProtectedRoute from './auth/ProtectedRoute.tsx';
+import SignInPage from './pages/SignInPage.tsx';
+import SignUpPage from './pages/SignUpPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route index element={<RecipesPage />} />
-                <Route path="add-recipe" element={<AddRecipePage />} />
-                <Route path="menus" element={<MenusPage />} />
-                <Route path="create-menu" element={<CreateMenuPage />} />
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="sign-in" element={<SignInPage />} />
+                    <Route path="sign-up" element={<SignUpPage />} />
+
+                    <Route element={<ProtectedRoute />}>
+                        <Route index element={<RecipesPage />} />
+                        <Route path="add-recipe" element={<AddRecipePage />} />
+                        <Route path="menus" element={<MenusPage />} />
+                        <Route path="create-menu" element={<CreateMenuPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     </StrictMode>
 );
